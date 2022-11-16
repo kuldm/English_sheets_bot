de24323a0df1f8d9da5b7d8dbfd2675288c9a13d
kuldm <prizrak2165@gmail.com>
2022-11-16 22:54:28 +0300

    Added library google sheets

--- Sheets_telebot.py
+++ Sheets_telebot.py
@@ -1,9 +1,14 @@
 import telebot
 from telebot import types
 
+from Google_sheets.AdminsAtWork import admin_at_work
+from Google_sheets.AdminsNames import admins_name
+from Google_sheets.AboutAdmins import about_admins
+
 
 with open("bots_token.txt", "r", encoding="cp1251") as f:
     bots_token = f.readline()
 
 
 bot = telebot.TeleBot(bots_token)
+
